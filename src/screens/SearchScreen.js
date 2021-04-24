7a98234eab2137ee988d0c73dd3e08ca33398608
Nickinfinity <nikhilg.me@gmail.com>
2021-04-24 20:06:11 +0530

    SeachScreen: Implement Search Functionality

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState('');
+	const [results, setResults] = useState([]);
+
+	const searchAPi = async () => {
+		const responses = await yelp.get('/search', {
+			params: {
+				limit: 50, //?limit=50 auto appended
+				term: term,
+				location: 'san jose',
+			},
+		});
+		setResults(responses.data.businesses);
+	};
+
 	return (
 		<View>
 			<SearchBar
 				term={term}
 				onTermChange={(searchKey) => setTerm(searchKey)}
-				onTermSubmit={() => console.log('Submitted')}
+				onTermSubmit={() => searchAPi()}
 			/>
+			<Text>We have found {results.length} results</Text>
 		</View>
 	);
 };
