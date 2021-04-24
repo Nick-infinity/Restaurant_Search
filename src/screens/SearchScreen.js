0bf483ac56c1f0d31a90ddf6b28be8bb7251d706
Nickinfinity <nikhilg.me@gmail.com>
2021-04-24 20:33:43 +0530

    SearchScreen: useEffect Hook for deafult search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,12 @@ const SearchScreen = () => {
 	const [results, setResults] = useState([]);
 	const [errorMsg, setError] = useState('');
 
-	const searchAPi = async () => {
+	const searchAPi = async (searchTerm) => {
 		try {
 			const responses = await yelp.get('/search', {
 				params: {
 					limit: 50, //?limit=50 auto appended
-					term: term,
+					term: searchTerm, //useState term is diff than this term -> this is api param
 					location: 'san jose',
 				},
 			});
@@ -23,12 +23,17 @@ const SearchScreen = () => {
 		}
 	};
 
+	// just to render some restaurants when app loads
+	useEffect(() => {
+		searchAPi('indian');
+	}, []);
+
 	return (
 		<View>
 			<SearchBar
 				term={term}
 				onTermChange={(searchKey) => setTerm(searchKey)}
-				onTermSubmit={() => searchAPi()}
+				onTermSubmit={() => searchAPi(term)} // this is state term
 			/>
 			{errorMsg ? (
 				<Text>{errorMsg}</Text>
